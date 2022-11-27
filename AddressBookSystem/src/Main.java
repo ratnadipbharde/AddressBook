531990a73aee4e5358179b29bc275ccf3f928ad7
Ratnadip Bharde <ratnadipbharde@gmail.com>
2022-11-27 18:34:14 +0530

    Added create a Contacts in Address Book

--- AddressBookSystem/src/Main.java
+++ AddressBookSystem/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("\t\t\tWelcome to Address Book Program\n" +
                 "--------------------------------------------------------");
+        Contact contact = new Contact();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First Name : ");
+        contact.setFirstName(sc.next());
+        System.out.println("Last Name : ");
+        contact.setLastName(sc.next());
+        System.out.println("Address : ");
+        contact.setAddress(sc.next());
+        System.out.println("City : ");
+        contact.setCity(sc.next());
+        System.out.println("State : ");
+        contact.setState(sc.next());
+        System.out.println("zip : ");
+        contact.setZip(sc.next());
+        System.out.println("Phone Number : ");
+        contact.setPhoneNumber(sc.next());
+        System.out.println("Email : ");
+        contact.setEmail(sc.next());
+        System.out.println(contact);
     }
 }
